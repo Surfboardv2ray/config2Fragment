1ee0e073bbec95959f987e7f41a73edf08c8a743
SurfboardV2ray <125461738+Surfboardv2ray@users.noreply.github.com>
2024-05-09 07:27:22 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
+import sys
 from v2tj import convert_uri_json
 
-uri = "vless://ce526092-81c1-42be-8354-88e58946c1f3@crackthecode.ftp.sh:443?encryption=none&security=tls&sni=vcp3.retroperitoneum.WORkeRs.dEV&alpn=h3%2Ch2%2Chttp%2F1.1&fp=chrome&type=ws&host=vcp3.retroperitoneum.WORkeRs.dEV&path=%2Fmonardas.aurorainiceland.com%3A443%2Fnimws#11%2B%F0%9F%8C%A8%F0%9F%87%A8%F0%9F%87%A6%2B%40Surfboardv2ray"
+uri = sys.argv[1]
 file = convert_uri_json(uri=uri)
