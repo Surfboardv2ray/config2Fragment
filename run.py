9ab5c156f9b4fffe38f0497bb36e9327fe865ee1
SurfboardV2ray <125461738+Surfboardv2ray@users.noreply.github.com>
2024-05-09 08:52:35 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-import os
 from v2tj import convert_uri_json
 
-uri = os.getenv('URI_SECRET')
+uri = "vless://149400e4-6910-4a3f-8c4a-c5c175929b87@crackthecode.ftp.sh:443?encryption=none&security=tls&sni=vcp3.dehel15354.wOrkeRS.dEv&alpn=h3%2Ch2%2Chttp%2F1.1&fp=ios&type=ws&host=vcp3.dehel15354.wOrkeRS.dEv&path=%2Fromb.minusober.com%3A443%2Fromb#13%2B%F0%9F%8C%A8%F0%9F%87%A8%F0%9F%87%A6%2B%40Surfboardv2ray"
 file = convert_uri_json(uri=uri)
