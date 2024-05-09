de51b444eb0b967a922f0282a00e6088d7f71115
SurfboardV2ray <125461738+Surfboardv2ray@users.noreply.github.com>
2024-05-09 10:33:46 -0400

    Update run.py

--- run.py
+++ run.py
@@ -7,4 +7,8 @@ with open('uris.txt', 'r') as f:
 
 # Loop through all URIs
 for uri in uris:
-    file = convert_uri_json(uri=uri.strip())
+    try:
+        file = convert_uri_json(uri=uri.strip())
+    except Exception as e:
+        print(f"Cannot parse URI: {uri.strip()}. Error: {str(e)}")
+        continue
