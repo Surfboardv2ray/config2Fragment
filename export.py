d384d11edde6e7c42b8b204f5c97a10bc2b16659
SurfboardV2ray <125461738+Surfboardv2ray@users.noreply.github.com>
2024-05-09 16:38:39 +0330

    Create export.py

---
+++ export.py
@@ -0,0 +1,28 @@
+import os
+import json
+
+# Directory containing the JSON files
+directory = '/configs'
+
+# List to store the contents of all JSON files
+combined = []
+
+# Loop through all files in the directory
+for filename in os.listdir(directory):
+    if filename.endswith('.json'):
+        # Full file path
+        filepath = os.path.join(directory, filename)
+
+        # Open and read the JSON file
+        with open(filepath, 'r') as f:
+            data = json.load(f)
+
+        # Add the content to the combined list
+        combined.append(data)
+
+# Write the combined content to a new JSON file
+with open('/configs/combined.json', 'w') as f:
+    f.write('[\n')
+    for item in combined:
+        f.write('    ' + json.dumps(item, indent=4) + ',\n')
+    f.write(']\n')
